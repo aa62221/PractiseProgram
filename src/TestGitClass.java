40c40d1765c8dd27492aecf2ecace137ca866da8
aa62221 <akshayathale.m09@gmail.com>
2025-01-26 01:35:26 +0530

    New commit

---
+++ src/TestGitClass.java
@@ -0,0 +1,10 @@
+
+public class TestGitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is to test the commit to github");
+	}
+
+}
